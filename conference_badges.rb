a8ec6501bbfe01fd303e2973daf5cf9f5f6334eb
sopharsogood <justanothersophie@gmail.com>
2021-07-20 18:29:49 -0400

    commit tree d76fa64327c6887cd889369b9ce1c3067e51b7d1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    badge_made = "Hello, my name is #{name}."
+    puts badge_made
+    badge_made
+end
+
+def batch_badge_creator(array)
+    multibadge = Array.new
+    array.each do |name|
+        multibadge << badge_maker(name)
+    end
+    multibadge
+end
+
+def assign_rooms(array)
+    room_assignments = Array.new
+    array.each_with_index do |name, index|
+        room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    room_assignments
+end
+
+def printer(array)
+    badgearray = batch_badge_creator(array)
+    roomsarray = assign_rooms(array)
+    roomsarray.each do |assignment|
+        puts assignment
+    end
+end
